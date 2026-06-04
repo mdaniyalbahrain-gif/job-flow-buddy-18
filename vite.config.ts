13153cf924db0ddcc87acd5eac28d0f68e6a6349
mdaniyalbahrain-gif <m.daniyalbahrain@gmail.com>
2026-06-05 03:10:34 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  tanstackStart: {
+    server: { entry: "server" },
   },
-  build: {
-    outDir: "dist",
+  vite: {
+    publicDir: "public",
   },
 });
